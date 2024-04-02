ea4b3d7ee213d70513ec9e86fb85a543a41da0cc
saulsantana0412 <saulsantan00412@gmail.com>
2024-04-02 11:57:56 -0400

    Se agregó el archivo [index.css] que contiene los estilos generales de toda la web.

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Librería SS</title>
+    <title>Librería Ss</title>
+    <link rel="stylesheet" href="estaticos\css\principal.css">
 </head>
 <body>
 
